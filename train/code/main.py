c4e62557c5eadd49f33c92b46e16acdcc89a548a
david <alireza.d159@outlook.com>
2024-08-13 10:38:10 +0330

    code

--- train/code/main.py
+++ train/code/main.py
@@ -3,10 +3,8 @@ from transformers import PreTrainedTokenizerFast
 from transformers import TextDataset, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 from datetime import datetime
-import torch
 import logging 
 
-print("thissss toorch" , torch.cuda.is_available())
 
 wrapped_tokenizer = PreTrainedTokenizerFast(
     tokenizer_file="tokenizer_BPE3.json",
