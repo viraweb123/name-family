2422c37f9ebdea997eb5a8b59c2aec03e4a85dbf
david <alireza.d159@outlook.com>
2024-08-14 18:00:37 +0330

    epoch and batch sizer changed

--- train/code/main.py
+++ train/code/main.py
@@ -24,8 +24,8 @@ logging.basicConfig(filename=os.path.join(log_dir, 'training.log'), level=loggin
 train_file_path_var = os.path.join(input_base_dir, 'test.txt')
 model_name_var = 'gpt2'
 overwrite_output_dir_var = True
-per_device_train_batch_size_var = 2
-num_train_epochs_var = 50
+per_device_train_batch_size_var = 100
+num_train_epochs_var = 32
 save_steps_var = 5000
 logging_steps = 100
 
