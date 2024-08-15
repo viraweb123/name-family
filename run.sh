c888fa9cd8d2f839520c6378a9be55afeb99ea9f
david <alireza.d159@outlook.com>
2024-08-15 11:25:37 +0330

    fixed

--- run.sh
+++ run.sh
@@ -22,7 +22,15 @@ echo "Setting permissions for 'train/' directory..."
 chmod 777 train/
 
 echo "Running the Docker container..."
-docker run --gpus all --rm -u $(id -u):$(id -g) -v $(pwd)/train:/train train:v1
+docker run \
+    --gpus all \
+    --rm \
+    -u $(id -u):$(id -g) \
+    -v $(pwd)/train:/train \
+    -e HF_HOME=/train/.cache/huggingface \
+    -w "/train/code" \
+    train:v1 \
+    python3 main.py
 
 if [ $? -ne 0 ]; then
   echo "Failed to run Docker container."
